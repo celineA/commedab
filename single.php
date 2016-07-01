e555cb1697bc87798d4f9b272ab6bfb6bef570d4
celineA <celine.amendola@gmail.com>
2016-07-01 14:33:49 +0100

    Update single.php

--- single.php
+++ single.php
@@ -1,44 +1,58 @@
 <?php
-	$args = array(
-		'post_type' => 'attachment',
-		'numberposts' => -1,
-		'post_status' => null,
-		'orderby' => 'menu_order',
-		'post_parent' => $post->ID
-	); 
-	$attachments = get_posts($args);
-	if (count($attachments) > 1) {
-		$attachments = array_reverse($attachments);
-		$i = 0;
-		foreach ($attachments as $attachment) {
-			if($attachment->ID != get_post_thumbnail_id($post->ID)){
-?>
-					
-	<div class="content" id="content-<?php echo $i; ?>">
-		<div class="content-img">
-			<div class="fleche prev"></div>
-			<div class="fleche next"></div>
-			<img src="<?php echo $attachment->guid; ?>" alt="" class="une-image">
-		</div>
-		<div class="content-desc" id="content-desc-<?php echo $i; ?>">
-			<h2><?php _e($attachment->post_title); ?></h2>
-		</div>
-	</div>
-                    
-<?php 
-	$i++;
-		    }
-			}
-		}
-?>
+	get_header();
+	
+	if ( have_posts() ) while ( have_posts() ) : the_post();?>
 
+	 <!-- Container (notice the relative width) :  -->
+        <div id="container3" style="width: 80%; max-width:1000px; height: 600px; background: #E0E0E0; margin:auto;"></div>
+    
+    	<?php 
 
+		$images = get_field('images');
 
-<?php 
-$images = get_field('images');     
-  if( $images ): ?>
+		if( $images ): ?>
+        
         <?php foreach( $images as $image ): ?>
-                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
-                <p><?php echo $image['caption']; ?></p>
-        <?php endforeach; ?>
-  <?php endif; ?>
+        <script>
+            $(function(){
+                var instance = $('#container3').Chocolat({
+                    setTitle : 'set title',
+                    loop: true,
+					enableZoom		:false,
+                    images : [
+                        {src : '<?php echo $image['sizes']['large']; ?>', title : '<?php echo $image['caption']; ?>'},
+                    ],
+                    imageSize : 'contain',
+                    container : '#container3'
+                }).data('chocolat');
+				   instance.api().open()
+                
+                $('.api-prev').on('click', function(e) {
+                    e.preventDefault()
+
+                    console.log('prev start');
+                    var def = instance.api().prev()
+
+                    def.done(function(){
+                        console.log('prev done');
+                    })
+                })
+                $('.api-next').on('click', function(e) {
+                    e.preventDefault()
+
+                    console.log('next start');
+                    var def = instance.api().next()
+
+                    def.done(function(){
+                        console.log('next done');
+                    })
+                })
+
+            });
+        </script>
+	<?php endforeach; ?>
+<?php endif; ?>
+
+<?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
